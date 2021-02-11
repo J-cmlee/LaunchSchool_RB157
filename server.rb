1341acb6804a7944028ee60e37940682fa6b2d8d
John Lee <john.cmlee@gmail.com>
2021-02-10 22:03:27 -0500

    refactoring code

--- server.rb
+++ server.rb
@@ -2,26 +2,40 @@
 
 require 'socket'
 
+def parse_request(request_line)
+  http_method, path_and_params, http = request_line.split(' ')
+  path, params, = path_and_params.split('?')
+  params = params.split('&').map { |ele| ele.split('=') }.to_h
+
+  [http_method, path, params]
+end
+
 server = TCPServer.new('localhost', 3003)
 loop do
   client = server.accept
 
   request_line = client.gets
   next if !request_line || request_line =~ /favicon/
-  
-  puts request_line
 
-  lines = request_line.split(' ')
-  http_method = lines.shift
-  path, query = lines.shift.split('?')
-  query = query.split('&').map { |ele| ele.split('=') }.to_h
+  puts request_line
 
   client.puts 'HTTP/1.1 200 OK'
   client.puts "Content-Type: text/plain\r\n\r\n"
   client.puts request_line
+
+  http_method, path, params = parse_request(request_line)
+
   client.puts 'HTTP method: ' + http_method
   client.puts 'path: ' + path
-  client.puts 'query: ' + query.to_s
+  client.puts 'params: ' + params.to_s
+
+  rolls = params['rolls'].to_i
+  sides = params['sides'].to_i
+
+  rolls.times do
+    roll = rand(sides) + 1
+    client.puts roll
+  end
 
   client.close
 end
